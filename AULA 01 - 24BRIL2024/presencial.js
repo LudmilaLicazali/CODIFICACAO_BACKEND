bbcad30a3a06ee56a126cf77156e556662b63c6e
LudmilaLicazali <112033592+LudmilaLicazali@users.noreply.github.com>
2024-04-25 20:53:00 -0300

    Continuação dos exercícios na aula presencial
    continuação de exercícios parte 1 e parte 2 na aula presencial

---
+++ AULA 01 - 24BRIL2024/presencial.js
@@ -0,0 +1,208 @@
+// ATIVIDADE PART 1
+
+
+// numero 1 - Crie  um  programa  que  exiba  no  console  uma  mensagem digitada pelo usuário.
+//let leia = require('prompt-sync') ()
+
+// let musicaFav = "Digite sua musica favorita?"
+
+// musicaFav = "Run"
+// console .log("Sua musica favorita é: " , musicaFav)
+
+
+// // numero 2 - Crie  um  programa  que  leia  o  nome  de  uma  pessoa  e  mostre uma mensagem de boas-vindas para ela. 
+
+// let nome = "Lucas"
+// console.log("Seja bem vindo" , nome,"!")
+
+
+// // numero 3 - Crie um programa que leia os dados de um aluno: matrícula, nome,  sobrenome,  CPF,  sexo,  data  de nascimento,  endereço e telefone e exiba no console.
+
+
+
+
+
+// //numero 4 - Crie  um  programa  que  leia  uma  disciplina,  um  aluno  e  as quatro notas desse aluno naquela disciplina. Ao final, mostre na tela a média do aluno na disciplina.
+
+
+// function media(){
+//     let nota1 = 7
+//     let nota2 = 8
+//     let nota3 = 10
+//     let nota4 = 4 
+   
+//     media = (nota1 + nota2 + nota3 + nota4 ) / 4
+   
+// }
+
+// console.log("A média do aluno Lucas na disciplina de matematica é: ", media);
+
+
+
+// //numero 5 -  Crie  um  programa  que  exiba  na  tela  a  tabuada  do  número digitado pelo usuário. 
+        
+//         // let prompt = require('prompt-sync')();
+
+//         // let valor;
+
+//         // valor = parseInt(prompt("Digite o valor da tabuada: "));
+
+//         // while(valor<=0){
+//         //     valor = parseInt(prompt(" O valor precisa ser maior que zero!, digite um novo valor. "));
+//         // }
+
+//         // for (i=1; i<=10; i++){    
+            
+//         //     console.log(valor + " x " + i + " = " + i*valor);
+
+//         // }
+
+
+// //numero 6 - Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+
+// let funcinario = "Euadardo"
+// let salario = 1000
+
+// conslole.log(funcinario, "recebe o salário no valor de  R$", salario, "reais")
+
+
+// //numero 7 - Crie  um  programa  que  leia  5  números  e  mostre  o  somatório entre eles.
+// function soma(){
+//     let valor1 = 2
+//     let valor2 = 8
+//     let valor3 = 10
+//     let valor4 = 4
+//     let valor5 = 4
+
+//     soma = (valor1 + valor2 + valor3 + valor4 + valor5 );
+// }
+
+// console.log(soma);
+
+// //numero 8 - Crie  um  programa  que  leia  a  idade  de  uma  pessoa  expressa em anos, meses e dias e mostre-a expressa apenas em dias
+// function idade () {
+//  let anos = 30
+//  let meses = 250
+//  let dias = 3500
+    
+//  anos = anos
+//  meses = (meses / 12)
+//  dias = (dias / 365)
+
+//  idade = dias
+ 
+// }
+
+// console.log(idade)
+
+
+// //numero 9 -  Crie  um  programa  que  leia  a  idade  de  uma  pessoa  expressa em dias e mostre-a expressa em anos, meses e dias.
+
+ 
+//   function idade2 (){
+//     let idade = 365
+//     calc = (idade / )
+//   }
+
+//   console.log()
+
+// //numero 10 - Crie  um  programa  que  leia  o  tempo  de  duração  de  uma atividade em horas, minutos e segundos e mostre-o expresso em segundos.
+
+
+
+// //numero 11 - Crie  um  programa  que  leia  o  tempo  de  duração  de  uma atividade em segundos e mostre-o expresso em horas, minutos e segundos.
+
+
+// //numero 12 - Crie um programa que leia o preço de um produto, calcule e mostre o seu preço promocional, com 15% de desconto.
+
+
+// function desconto (){
+//     let preco = 80
+    
+//     calculo = (preco * 15 / 100)
+//     desconto = (preco - calculo)
+
+// }
+
+// console.log (desconto)
+
+
+
+//numero 13 - Crie um programa que leia o salário de um funcionário, calcule e mostre o seu novo salário, com 27,50% de aumento.
+
+        // function aumento (){
+        //     let salario = 1500
+            
+        //     calculo = (salario * 27,50 / 100)
+        //     aumento = (salario + calculo)
+
+        // }
+
+        // console.log (aumento)
+
+
+
+//numero 14 - Crie um programa que leia o valor da matrícula de um curso, calcule e mostre o seu novo valor, com 20% de desconto.
+
+
+//numero 15 - Crie  um  programa  que  leia  o  valor  de  um  boleto  que  será pago  com  atraso,  a  quantidade  de  dias  em  atraso,  calcule  e mostre o seu novo valor com uma taxa de 1,25% ao dia.
+
+
+
+// ATIVIDADE PART 2
+
+//numero 1 - Crie  um  programa  que  gera  e  escreve  os  números  ímpares entre 0 e 50. No início da 30ª iteração o sistema deve sair do laço de repetição.
+
+
+//     contador = 0
+//    for (let i=1; i<=50; i+=1){
+    
+//     cosole.log (i)
+// }
+
+//numero 2 - Crie um programa que gera e escreve os números pares entre 0  e  50.  No  início  da  30ª  iteração  o  sistema  deve  pular  o  laço de repetição.
+
+
+
+//numero 3 - Crie um programa para receber um número qualquer e exibir no console se é par ou ímpar.
+
+
+
+// numero = prompt("Digite um numero");
+
+            // let numero = 20
+
+            // if (numero % 2 == 0){
+
+            // console.log("O numero é par");
+
+            // }
+
+            // else {
+            //     console.log("O numero é impar");
+            // }
+
+
+//numero 4 - Crie um programa que leia um valor inicial A e exiba a sequência de valores do cálculo de A! 
+//e o seu resultado. Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120 
+
+// let numero =  5
+
+// while (numero >1 <5 ) {
+//     numero = numero 
+//     console.log (numero);
+   
+
+// }
+
+numero =  5
+do { 
+    numero = (i--)
+
+    
+}
+while (
+    numero <=5
+)
+
+console.log(numero)
