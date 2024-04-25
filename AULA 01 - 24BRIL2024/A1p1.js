3e15d268c46defd4b58ba47506c273286b555d15
Ludmila <ludmilalicazali@gmail.com>
2024-04-24 22:47:51 -0300

    Inincio do repositorio e inclusão dos arquivos da primeira aula no dia 24 de abril de 2024

---
+++ AULA 01 - 24BRIL2024/A1p1.js
@@ -0,0 +1,107 @@
+// ATIVIDADE PART 1
+
+
+// numero 1 - Crie  um  programa  que  exiba  no  console  uma  mensagem digitada pelo usuário.
+//let leia = require('prompt-sync') ()
+
+let musicaFav = "Digite sua musica favorita?"
+
+musicaFav = "Run"
+console .log("Sua musica favorita é: " , musicaFav)
+
+
+// numero 2 - Crie  um  programa  que  leia  o  nome  de  uma  pessoa  e  mostre uma mensagem de boas-vindas para ela. 
+
+let nome = "Lucas"
+console.log("Seja bem vindo" , nome,"!")
+
+
+// numero 3 - Crie um programa que leia os dados de um aluno: matrícula, nome,  sobrenome,  CPF,  sexo,  data  de nascimento,  endereço e telefone e exiba no console.
+
+
+
+
+
+//numero 4 - Crie  um  programa  que  leia  uma  disciplina,  um  aluno  e  as quatro notas desse aluno naquela disciplina. Ao final, mostre na tela a média do aluno na disciplina.
+
+
+function media(){
+    let nota1 = 7
+    let nota2 = 8
+    let nota3 = 10
+    let nota4 = 4 
+   
+    media = (nota1 + nota2 + nota3 + nota4 ) / 4
+   
+}
+
+console.log("A média do aluno Lucas na disciplina de matematica é: ", media);
+
+
+
+//numero 5 -  Crie  um  programa  que  exiba  na  tela  a  tabuada  do  número digitado pelo usuário. 
+        
+        // let prompt = require('prompt-sync')();
+
+        // let valor;
+
+        // valor = parseInt(prompt("Digite o valor da tabuada: "));
+
+        // while(valor<=0){
+        //     valor = parseInt(prompt(" O valor precisa ser maior que zero!, digite um novo valor. "));
+        // }
+
+        // for (i=1; i<=10; i++){    
+            
+        //     console.log(valor + " x " + i + " = " + i*valor);
+
+        // }
+
+
+//numero 6 - Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+
+let funcinario = "Euadardo"
+let salario = 1000
+
+conslole.log(funcinario, "recebe o salário no valor de  R$", salario, "reais")
+
+
+//numero 7 - Crie  um  programa  que  leia  5  números  e  mostre  o  somatório entre eles.
+function soma(){
+    var valor1 = 2
+    var valor2 = 8
+    var valor3 = 10
+    var valor4 = 4
+    var valor5 = 4
+
+    soma = (valor1 + valor2 + valor3 + valor4 + valor5 );
+}
+
+console.log(soma);
+
+//numero 8 - Crie  um  programa  que  leia  a  idade  de  uma  pessoa  expressa em anos, meses e dias e mostre-a expressa apenas em dias
+function idade () {
+ let anos = 30
+ let meses = 250
+ let dias = 3500
+    
+ anos = anos
+ meses = (meses / 12)
+ dias = (dias / 365)
+
+ idade = dias
+ 
+}
+
+console.log(idade)
+
+
+//numero 9 -  Crie  um  programa  que  leia  a  idade  de  uma  pessoa  expressa em dias e mostre-a expressa em anos, meses e dias.
+
+ 
+  function idade2 (){
+    let idade = 
+    
+
+
+  }
